ae1416df09ca10d9f730299bcf406506246119ca
sh360659 <sharad.rana@wipro.com>
2019-08-29 12:48:55 +0530

    @a
    commit
    @a
    
    df
    sadf
    wq

---
+++ Groovy/src/com/groovy/automation/Hello.groovy
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.groovy.automation
+
+/**
+ * @author SH360659
+ *
+ */
+class Hello {
+
+	/**
+	 * @param args
+	 */
+	static void main(args) {
+		// TODO Auto-generated method stub
+		
+	}
+}
