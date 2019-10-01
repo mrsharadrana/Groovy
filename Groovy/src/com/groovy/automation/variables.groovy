b2a225eb173d7aa76848740907ca31efeaf07b57
sh360659 <sharad.rana@wipro.com>
2019-10-01 15:33:16 +0530

    Day 1

---
+++ Groovy/src/com/groovy/automation/variables.groovy
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.groovy.automation
+
+/**
+ * @author SH360659
+ *
+ */
+class variables {
+
+	/**
+	 * @param args
+	 */
+	static void main(args) {
+		// TODO Auto-generated method stub
+		def name = "sharad"
+		println(name)
+		println'My name is $name'
+		println "My name is $name"
+	}
+}
